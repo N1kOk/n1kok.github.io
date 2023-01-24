8bd5d85c87e18247ffa772c75b97e62326dafd2a
N1kOk <n1k_ok@bk.ru>
2023-01-24 13:08:45 +0500

    feat: add icon links

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
 	<TheHeader/>
 
 	<main>
-		<section class="h-[1px] min-h-screen bg-[radial-gradient(at_50%_0,#224356,#222b3a)]">
-			<div class="container flex max-lg:flex-col gap-4 h-full min-h-[15rem]">
+		<section class="flex items-center min-h-screen h-max bg-[radial-gradient(at_50%_0,#224356,#222b3a)]">
+			<div class="container flex max-lg:flex-col gap-4 min-h-[20rem]">
 				<div class="flex flex-center w-full text-5xl">
 					<div>
 						<div class="font-bold">Frontend</div>
@@ -12,59 +12,59 @@
 				</div>
 				<div class="flex flex-center w-full">
 					<div class="relative w-[15rem] h-[15rem] scale-150 select-none">
-						<IconInteractive class="left-1/2 top-1/2 scale-[2]">
+						<IconInteractive class="left-1/2 top-1/2 scale-[2]" href="https://vuejs.org/">
 							<img src="/assets/images/vue.svg" alt="logo">
 						</IconInteractive>
 
-						<IconInteractive class="left-[30%] top-1/2">
+						<IconInteractive class="left-[30%] top-1/2" href="https://developer.mozilla.org/en-US/docs/Web/JavaScript">
 							<img src="/assets/images/js.svg" alt="logo">
 						</IconInteractive>
 
-						<IconInteractive class="left-[70%] top-1/2">
+						<IconInteractive class="left-[70%] top-1/2" href="https://www.typescriptlang.org/">
 							<img src="/assets/images/ts.svg" alt="logo">
 						</IconInteractive>
 
-						<IconInteractive class="left-1/2 top-[30%]">
+						<IconInteractive class="left-1/2 top-[30%]" href="https://nuxt.com/">
 							<img src="/assets/images/nuxt.svg" alt="logo">
 						</IconInteractive>
 
-						<IconInteractive class="left-1/2 top-[70%]">
+						<IconInteractive class="left-1/2 top-[70%]" href="https://tailwindcss.com/">
 							<img src="/assets/images/tailwind.svg" alt="logo">
 						</IconInteractive>
 
-						<IconInteractive class="left-1/3 top-1/3">
+						<IconInteractive class="left-1/3 top-1/3" href="https://git-scm.com/">
 							<img src="/assets/images/git.svg" alt="logo">
 						</IconInteractive>
 
-						<IconInteractive class="left-2/3 top-1/3">
+						<IconInteractive class="left-2/3 top-1/3" href="https://webpack.js.org/">
 							<img src="/assets/images/webpack.svg" alt="logo">
 						</IconInteractive>
 
-						<IconInteractive class="left-1/3 top-2/3">
+						<IconInteractive class="left-1/3 top-2/3" href="https://vitejs.dev/">
 							<img src="/assets/images/vite.svg" alt="logo">
 						</IconInteractive>
 
-						<IconInteractive class="left-2/3 top-2/3">
+						<IconInteractive class="left-2/3 top-2/3" href="https://sass-lang.com/">
 							<img src="/assets/images/sass.svg" alt="logo">
 						</IconInteractive>
 
-						<IconInteractive class="left-[22%] top-[22%]">
+						<IconInteractive class="left-[22%] top-[22%]" href="https://postcss.org/">
 							<img src="/assets/images/postcss.svg" alt="logo">
 						</IconInteractive>
 
-						<IconInteractive class="left-[78%] top-[22%]">
+						<IconInteractive class="left-[78%] top-[22%]" href="https://www.docker.com/">
 							<img src="/assets/images/docker.svg" alt="logo">
 						</IconInteractive>
 
-						<IconInteractive class="left-[22%] top-[78%]">
+						<IconInteractive class="left-[22%] top-[78%]" href="https://jestjs.io/">
 							<img src="/assets/images/jest.svg" alt="logo">
 						</IconInteractive>
 
-						<IconInteractive class="left-[78%] top-[78%]">
+						<IconInteractive class="left-[78%] top-[78%]" href="https://storybook.js.org/">
 							<img src="/assets/images/storybook.svg" alt="logo">
 						</IconInteractive>
 
-						<IconInteractive class="left-1/2 top-[85%]">
+						<IconInteractive class="left-1/2 top-[85%]" href="https://nodejs.org/en/about/">
 							<img src="/assets/images/nodejs.svg" alt="logo">
 						</IconInteractive>
 					</div>
--- src/components/IconInteractive.vue
+++ src/components/IconInteractive.vue
@@ -1,7 +1,8 @@
 <template>
 	<a
-		class="absolute -translate-x-1/2 -translate-y-1/2 w-6 h-6 transition-transform hover:brightness-150"
+		class="absolute -translate-x-1/2 -translate-y-1/2 w-6 h-6 transition-transform hover:brightness-125"
 		href="#"
+		rel="nofollow"
 		target="_blank"
 	>
 		<slot/>
