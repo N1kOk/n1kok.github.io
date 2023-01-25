750d7c4e337d761d57bad654d59c3d07d0486687
N1kOk <n1k_ok@bk.ru>
2023-01-25 16:51:29 +0500

    feat: add icon animation

--- src/components/IconInteractive.vue
+++ src/components/IconInteractive.vue
@@ -1,10 +1,28 @@
 <template>
 	<a
-		class="absolute -translate-x-1/2 -translate-y-1/2 w-6 h-6 transition-transform hover:brightness-125"
+		class="link group"
 		href="#"
 		rel="nofollow"
 		target="_blank"
 	>
-		<slot/>
+		<div class="icon">
+			<slot/>
+		</div>
 	</a>
 </template>
+
+<style scoped>
+.link {
+	@apply absolute w-6 h-6 -translate-x-1/2 -translate-y-1/2;
+}
+
+.icon {
+	--rotate: 0deg;
+
+	transform: rotateY(var(--rotate));
+
+	@apply
+	absolute w-6 h-6
+	will-change-transform transition-all duration-700 group-hover:brightness-150 group-hover:[--rotate:360deg];
+}
+</style>
